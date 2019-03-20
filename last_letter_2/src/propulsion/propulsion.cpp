d8f2c0e2fbcfafebcf4c5361a7bbfeafeb747300
milistefanos <milistefanos@hotmail.com>
2019-03-20 14:02:48 +0200

    Useless code
    Delete useless files and lines in code

--- last_letter_2/src/propulsion/propulsion.cpp
+++ last_letter_2/src/propulsion/propulsion.cpp
@@ -22,9 +22,7 @@ void Propulsion::rotateWind()
     v_in.vector.x = model->airdata.wind_x;
     v_in.vector.y = model->airdata.wind_y;
     v_in.vector.z = model->airdata.wind_z;
-    // std::cout<<"frame="<<v_in.header.frame_id<<"    x="<<v_in.vector.x<<" y="<<v_in.vector.y<<" z="<<v_in.vector.z<<std::endl;
     v_out = tfBuffer.transform(v_in, motor_name_link);
-    // std::cout<<"frame="<<v_out.header.frame_id<<"         x="<<v_out.vector.x<<" y="<<v_out.vector.y<<" z="<<v_out.vector.z<<std::endl<<std::endl;
 
     normalWind = v_out.vector.x;
     if (!std::isfinite(normalWind)) { ROS_FATAL("propulsion.cpp: NaN value in normalWind"); ros::shutdown(); }
